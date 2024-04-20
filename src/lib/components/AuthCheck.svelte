f1fa96eab052c82fc565ac7c2e86891a2cd48d43
s0ru <apazgarcia23@outlook.es>
2024-04-21 00:41:01 +0200

    add authcheck component

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { user } from '$lib/firebase'
+</script>
+
+{#if user}
+    <slot></slot>
+{:else}
+    <p>You must be logged in to view this page. <a href="/login">LOGIN</a></p>
+{/if}
+
+<style>
+    p > a {
+        color: var(--detail-color);
+    }
+</style>
