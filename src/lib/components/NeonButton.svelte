347c519940257433f6d868e19c8a7a3c21c270b8
s0ru <apazgarcia23@outlook.es>
2024-04-21 02:14:51 +0200

    update button style

--- src/lib/components/NeonButton.svelte
+++ src/lib/components/NeonButton.svelte
@@ -16,15 +16,16 @@
         max-width: 30vw;
         height: 7.5vh;
         color: white;
-        background-color: var(--detail-color);
+        background-color: transparent;
         border-radius: 10px;
         border: none;
         outline-style: solid;
         outline-width: medium;
-        outline-color: white;
+        outline-color: var(--detail-color);
+        box-shadow: 0 0 10px var(--accent-color);
     }
 
     button:active{
-        background-color: var(--accent-color);
+        box-shadow: inset 0 0 100px -50px var(--accent-color);
     }
 </style>
