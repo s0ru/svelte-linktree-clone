04f394ac90f339f2e1282f27e58194fd3218505f
s0ru <apazgarcia23@outlook.es>
2024-04-27 12:04:15 +0200

    add user link component

---
+++ src/lib/components/UserLink.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let icon = 'default'
+    export let url = 'foo'
+    export let title = 'fancy title'
+</script>
+
+<a href={url}>
+    <img src={`/${icon}.png`} alt={icon} width="32" height="32"/>
+    <span>{title}</span>
+</a>
