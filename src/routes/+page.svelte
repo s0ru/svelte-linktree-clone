3be9b8d192d8af911747223d42eab9774b23adae
s0ru <apazgarcia23@outlook.es>
2024-04-28 13:26:33 +0200

    add landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import NeonButton from "$lib/components/NeonButton.svelte";
+    import { auth, userData, user } from "$lib/firebase";
+    import { signOut } from 'firebase/auth'
+</script>
+
+<main>
+    <h1>One URL</h1>
+    <p class="error">To rule them all</p>
+    <div class="landing-btns">
+        {#if !$user}
+            <a href="/login"><NeonButton click={() => {}}>LOGIN</NeonButton></a>
+        {:else}
+            <a href="/{$userData?.username}"><NeonButton click={() => {}}>@{$userData?.username}</NeonButton></a>
+            <NeonButton click={() => signOut(auth)}>LOGOUT</NeonButton>
+        {/if}
+    </div>
+</main>
+
+<style>
+    main{
+        width: 100%;
+        height: 90vh;
+        display: flex;
+        flex-direction: column;
+        gap: 0.2em;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .error{
+        color: #FF3131;
+        text-shadow: 1px -1px 5px #FF3131;
+    }
+
+    .landing-btns{
+        display: flex;
+        gap: 1em;
+    }
+</style>
