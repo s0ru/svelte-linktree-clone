02b97c4e6d00f36225e045e849d888c6d6d103e2
s0ru <apazgarcia23@outlook.es>
2024-04-30 20:51:34 +0200

    add vercel speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
     import { user, userData } from '$lib/firebase';
     import '../base.css'
 
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+    injectSpeedInsights();
+
     $user;
     $userData;
 </script>
